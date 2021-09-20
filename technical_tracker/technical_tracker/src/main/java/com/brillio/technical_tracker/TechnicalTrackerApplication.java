ad1700f82d88dd3fa1e3307a421e90032090f699
Ashish Amar <ashishamar1999@gmail.com>
2021-09-20 12:51:03 +0530

    Implemented API Gateway and Routes with filters.

--- technical_tracker/technical_tracker/src/main/java/com/brillio/technical_tracker/TechnicalTrackerApplication.java
+++ technical_tracker/technical_tracker/src/main/java/com/brillio/technical_tracker/TechnicalTrackerApplication.java
@@ -1,13 +1,42 @@
 package com.brillio.technical_tracker;
 
+import com.brillio.technical_tracker.filter.ErrorFilter;
+import com.brillio.technical_tracker.filter.PostFilter;
+import com.brillio.technical_tracker.filter.PreFilter;
+import com.brillio.technical_tracker.filter.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
+@RefreshScope
 public class TechnicalTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechnicalTrackerApplication.class, args);
+	}
+
+	@Bean
+	public PostFilter getPostFilter(){
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter getErrorFilter(){
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public PreFilter getPreFilter(){
+		return new PreFilter();
+	}
+
+	@Bean
+	public RouteFilter getRouteFilter(){
+		return new RouteFilter();
 	}
 
 }
